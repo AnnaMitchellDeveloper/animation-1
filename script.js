e04f22618da1ecdfbe3e42674521f5650951e3bf
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-05-08 10:45:14 -0400

    Added animation on the page

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.from(".link", {opacity:0, duration:1, delay:1, stagger:0.6})
+gsap.from(".chocolate", {y:70, opacity: 0, duration: 2, delay: 3})
+gsap.from(".btn", {y:30, opacity: 0, duration: 4, delay: 4 })
